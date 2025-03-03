bf1f6d70fe35cc310a185242079b005a41e2aeed
SimoneChiodo <simochi.005@gmail.com>
2025-03-03 14:22:06 +0100

    Add Televisore with: variables, constructors, getter, setter; Add main Televisore test

--- src/org/lesson/shop/Main.java
+++ src/org/lesson/shop/Main.java
@@ -6,8 +6,15 @@ public class Main {
   public static void main(String[] args) {
     Smartphone telefono = new Smartphone("Telephones", "Samzung", new BigDecimal(200), new BigDecimal(0.22f), 120);
 
+    System.out.println("Some Smartphone info: ");
     System.out.println(telefono.getNome());
-    System.out.println(telefono.getCode());
+    System.out.println(telefono.getCodeIMEI());
+
+    Televisore televisione = new Televisore("Telvision", "LGS", new BigDecimal(200), new BigDecimal(0.22), false, 22f, 80.3f, 2.2f);
+
+    System.out.println("\nSome Television info: ");
+    System.out.println(televisione.getNome());
+    System.out.println(televisione.getIsSmart() ? "It's smart" : "It's not smart");
 
   }
 }
---
+++ src/org/lesson/shop/Televisore.java
@@ -0,0 +1,78 @@
+package org.lesson.shop;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Televisore extends Prodotto {
+  
+  private float height;
+  private float width;
+  private float depth;
+  private boolean isSmart;
+
+  Televisore(){
+    // Call the superclass Constructor
+    super();
+
+    // Generate random mesures
+    this.height = generateMesure();
+    this.width = generateMesure();
+    this.depth = generateMesure();
+
+    //Assign other values
+    this.isSmart = isSmart;
+  }
+  
+  Televisore(String nome, String marca, BigDecimal prezzo, BigDecimal iva, Boolean isSmart, float height, float width, float depth){
+    // Call the superclass Constructor
+    super(nome, marca, prezzo, iva);
+
+    // Generate a new random code
+    this.isSmart = isSmart;
+
+    //Assign other values
+    this.height = height;
+    this.width = width;
+    this.depth = depth;
+  }
+
+
+  // Getters & Setters
+  public float getHeight(){
+    return this.height;
+  }
+  public void setHeight(float height){
+    this.height = height;
+  }
+  
+  public float getWidth(){
+    return this.width;
+  }
+  public void setWidth(float width){
+    this.width = width;
+  }
+  
+  public float getDepth(){
+    return this.depth;
+  }
+  public void setDepth(float depth){
+    this.depth = depth;
+  }
+
+  public boolean getIsSmart(){
+    return this.isSmart;
+  }
+  public void setIsSmart(boolean isSmart){
+    this.isSmart = isSmart;
+  }
+
+
+  // Functions
+  private float generateMesure(){
+    Random random = new Random();
+
+    // Generate a new random code
+    return random.nextFloat(152.99f);
+  }
+
+}
