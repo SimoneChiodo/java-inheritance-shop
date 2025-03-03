53fb5df67f192d523697ab8b2726e23d69971471
SimoneChiodo <simochi.005@gmail.com>
2025-03-03 14:07:08 +0100

    Add Smartphone with: variables, constructors, getter, setter; Add main Smartphone test

--- src/org/lesson/shop/Main.java
+++ src/org/lesson/shop/Main.java
@@ -1,7 +1,13 @@
 package org.lesson.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
   public static void main(String[] args) {
-    
+    Smartphone telefono = new Smartphone("Telephones", "Samzung", new BigDecimal(200), new BigDecimal(0.22f), 120);
+
+    System.out.println(telefono.getNome());
+    System.out.println(telefono.getCode());
+
   }
 }
---
+++ src/org/lesson/shop/Smartphone.java
@@ -0,0 +1,55 @@
+package org.lesson.shop;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Smartphone extends Prodotto {
+  
+  private int codeIMEI;
+  private int storageAmount;
+
+  Smartphone(){
+    // Call the superclass Constructor
+    super();
+
+    // Generate a new random code
+    this.codeIMEI = generateIMEICode();
+  }
+  
+  Smartphone(String nome, String marca, BigDecimal prezzo, BigDecimal iva, int storageAmount){
+    // Call the superclass Constructor
+    super(nome, marca, prezzo, iva);
+
+    // Generate a new random code
+    this.codeIMEI = generateIMEICode();
+
+    //Assign other values
+    this.storageAmount = storageAmount;
+  }
+
+
+  // Getters & Setters
+  public int getCodeIMEI(){
+    return this.codeIMEI;
+  }
+  public void setCodeIMEI(int codeIMEI){
+    this.codeIMEI = codeIMEI;
+  }
+
+  public int getStorageAmount(){
+    return this.storageAmount;
+  }
+  public void setStorageAmount(int storageAmount){
+    this.storageAmount = storageAmount;
+  }
+
+
+  // Functions
+  private int generateIMEICode(){
+    Random random = new Random();
+
+    // Generate a new random code
+    return random.nextInt(999999999);
+  }
+
+}
