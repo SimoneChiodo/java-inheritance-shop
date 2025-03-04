dc3ca206de9c1940959cf430cf9722ca320fb6a7
SimoneChiodo <simochi.005@gmail.com>
2025-03-04 14:09:46 +0100

    Add bonus

--- src/org/lesson/shop/Carrello.java
+++ src/org/lesson/shop/Carrello.java
@@ -1,6 +1,7 @@
 package org.lesson.shop;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Carrello {
@@ -12,8 +13,21 @@ public class Carrello {
 
     Scanner scanner = new Scanner(System.in);
     String choice2 = "y", carrello = "";
+    boolean tessera = false;
+    BigDecimal totaleSpesa = new BigDecimal(0);
 
     for(int i = 1; choice2.equals("y"); i++){
+
+      String choise3 = "y";
+      do{
+        if(!choise3.equals("y") && !choise3.equals("n"))
+        System.out.println("\nIl valore inserito non e' accettato!");
+        System.out.println("\nHai la tessera fedelta'? (Y/N)");
+        choise3 = scanner.nextLine();
+        if(choise3.toLowerCase() == "y")
+          tessera = true;
+      } while(!choise3.equals("y") && !choise3.equals("n"));
+      
       System.out.println("\nInserisci un prodotto: \n1- Smartphone \n2- Televisore \n3- Cuffie");
 
       int choice = scanner.nextInt();
@@ -34,6 +48,13 @@ public class Carrello {
             scanner.nextLine(); // This line consumes the \n character
 
             Smartphone smartphone = new Smartphone(nome, marca, prezzo, iva, storageAmount);
+
+            if(tessera && storageAmount <= 32)
+              smartphone.setPrezzo(calcolaSconto(prezzo, new BigDecimal(5)));
+            else if(tessera) 
+              smartphone.setPrezzo(calcolaSconto(prezzo, new BigDecimal(2)));
+
+            totaleSpesa = totaleSpesa.add(smartphone.getPrezzo());
             carrello += "\n" + i + " - " + smartphone.toString();
             break;
 
@@ -62,6 +83,13 @@ public class Carrello {
             boolean isSmart = isSmartString == "y" ? true : false;
             
             Televisore televisore = new Televisore(nome, marca, prezzo, iva, isSmart, height, width, depth);
+
+            if(tessera && isSmart == false)
+              televisore.setPrezzo(calcolaSconto(prezzo, new BigDecimal(10)));
+            else if(tessera) 
+              televisore.setPrezzo(calcolaSconto(prezzo, new BigDecimal(2)));
+
+              totaleSpesa = totaleSpesa.add(televisore.getPrezzo());
             carrello += "\n" + i + " - " + televisore.toString();
             break;
 
@@ -86,6 +114,13 @@ public class Carrello {
             boolean isWireless = isWirelessString == "y" ? true : false;
 
             Cuffie cuffie = new Cuffie(nome, marca, prezzo, iva, color, isWireless);
+
+            if(tessera && isWireless == false)
+              cuffie.setPrezzo(calcolaSconto(prezzo, new BigDecimal(7)));
+            else if(tessera) 
+              cuffie.setPrezzo(calcolaSconto(prezzo, new BigDecimal(2)));
+
+            totaleSpesa = totaleSpesa.add(cuffie.getPrezzo());
             carrello += "\n" + i + " - " + cuffie.toString();
             break;
       }
@@ -96,12 +131,18 @@ public class Carrello {
         System.out.println("\n\nVuoi inserire un altro prodotto? (Y/N)");
         choice2 = scanner.nextLine();
         choice2 = choice2.toLowerCase();
-        System.out.println(choice2);
       } while(!choice2.equals("y") && !choice2.equals("n"));
     }
     
     scanner.close();
     System.out.println("\n\nResoconto del carrello: " + carrello);
+    System.out.println("\n----------\nIl totale per la spesa e': \n" + totaleSpesa + " euro");
 
+  }
+
+  private static BigDecimal calcolaSconto(BigDecimal prezzo, BigDecimal scontoPercentuale){
+      BigDecimal sconto = prezzo.multiply(scontoPercentuale).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP);
+      BigDecimal prezzoScontato = prezzo.subtract(sconto);
+      return prezzoScontato;
   }
 }
